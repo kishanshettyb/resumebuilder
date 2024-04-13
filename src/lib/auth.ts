fdad63c0ff7df92fa9d32d557b31e81158eab011
kishanshettyb <kishanqr@gmail.com>
2024-04-13 23:54:31 +0530

    feat: add google authentication

---
+++ src/lib/auth.ts
@@ -0,0 +1,18 @@
+import { NextAuthOptions, getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+
+import GoogleProvider from 'next-auth/providers/google';
+
+export const authConfig: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+    })
+  ]
+};
+
+export async function loginIsRequiredServer() {
+  const session = await getServerSession(authConfig);
+  if (!session) return redirect('/');
+}
