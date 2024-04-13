93bcfbc84f303c918833fdedbee3cb62330de49d
kishanshettyb <kishanqr@gmail.com>
2024-04-13 23:42:08 +0530

    refactor: update style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import Navbar from '@/components/navbar';
+
 export default function Home() {
   return (
     <div>
       <Navbar />
       <div className="container mx-auto">
         <div className="w-4/5 my-48">
-          <h1 className="text-slate-700 dark:text-slate-50 mb-5 text-6xl text-center font-bold">
+          <h1 className="mb-5 text-6xl font-bold text-center text-slate-700 dark:text-slate-50">
             Unlock
             <br /> Your Career Potential
           </h1>
-          <h2 className="text-8xl w-full text-center text-green-600  font-bold">
+          <h2 className="w-full font-bold text-center text-green-600 text-8xl">
             Create a Winning Resume Today!
           </h2>
         </div>
