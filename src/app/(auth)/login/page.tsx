cb12eb1b498f363b8654f295b6643e0206477765
kishanshettyb <kishanqr@gmail.com>
2024-04-13 23:51:02 +0530

    feat: add google login button to login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,11 +1,19 @@
+import { GoogleSignInButton } from '@/components/googleSignInButton';
 import React from 'react';
+import { authConfig } from '@/lib/auth';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 
 type Props = {};
 
-export default function Login({}: Props) {
+export default async function Login({}: Props) {
+  const session = await getServerSession(authConfig);
+  console.log('Session: ', session);
+  if (session) return redirect('/');
   return (
     <div className="container">
-      <h1 className="my-40 text-slate-900 font-bold text-6xl text-center">Login</h1>
+      <h1 className="my-40 text-6xl font-bold text-center text-slate-900">Login</h1>
+      <GoogleSignInButton />
     </div>
   );
 }
